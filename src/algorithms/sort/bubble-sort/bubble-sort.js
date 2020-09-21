eaa11da4c43b5d0071c7840aa0fb6fd748adab9b
Vadim Obanin <mortido3@mail.ru>
2020-09-21 18:29:59 +0300

    rename folders

--- src/algorithms/sort/bubble-sort/bubble-sort.js
+++ src/algorithms/sort/bubble-sort/bubble-sort.js
@@ -1,2 +1,11 @@
 "use strict"
-
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let endSort = arr.length;
+        for (let j = 0; j<arr.length - 1; j++ ) {
+            if (endSort > 0) {
+                
+            }
+        }
+    }
+}
